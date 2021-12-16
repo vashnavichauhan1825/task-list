7b72fe35914d3e02460dc1f5ad37b6e474998865
vashnavichauhan1825 <70444778+vashnavichauhan1825@users.noreply.github.com>
2021-12-16 18:40:08 +0530

    update comment in index.js

--- index.js
+++ index.js
@@ -59,24 +59,7 @@ app.post('/create-task', function(req, res){
 });
 
 
-// deleting Tasks
-// app.get('/delete-task', function(req, res){
-//     // get the id from query
-//     var id = req.query;
 
-//     // checking the number of tasks selected to delete
-//     var count = Object.keys(id).length;
-//     for(let i=0; i < count ; i++){
-        
-//         // finding and deleting tasks from the DB one by one using id
-//         Task.findByIdAndDelete(Object.keys(id)[i], function(err){
-//         if(err){
-//             console.log('error in deleting task');
-//             }
-//         })
-//     }
-//     return res.redirect('back'); 
-// });
 
 // delete item from the table
 app.get('/delete-item/', function(req,res){
